d4a787ed7cb86c32b2ced29322d06841f7c95f44
DPR1984_brfr <danielpupio@hotmail.com>
2026-05-06 16:39:32 +0200

    Add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import WorldCup2026Simulator from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WorldCup2026Simulator />
+  </React.StrictMode>
+);
